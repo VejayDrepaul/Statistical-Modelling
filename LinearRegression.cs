2adf3b87a25c7e3c99488ad3fc24c951b356600a
Vejay Drepaul <vejayddrepaul@gmail.com>
2023-12-14 23:51:57 -0500

    Basic linear regression complete

--- LinearRegression.cs
+++ LinearRegression.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Transactions;
 
 namespace Statistical_Modelling
 {
@@ -16,6 +18,7 @@ namespace Statistical_Modelling
             this._yData = _yData;
             this._n = _xData.Length;
             SampleMeans();
+            LeastSquaresCoefficients();
         }
 
         private void SampleMeans()
@@ -32,7 +35,7 @@ namespace Statistical_Modelling
             this._yBar = ySum / this._n;
         }
 
-        public double[] LeastSquaresCoefficients()
+        public void LeastSquaresCoefficients()
         {
             double numerator = 0;
             double denominator = 0;
@@ -45,8 +48,78 @@ namespace Statistical_Modelling
 
             this.betaOne = numerator / denominator;
             this.betaZero = this._yBar - (this.betaOne * this._xBar);
+        }
 
-            return new double[] { this.betaOne, this.betaOne };
+        public double StandardDeviation()
+        {
+            double numerator = 0;
+
+            for (int i = 0; i < this._n; i++)
+            {
+                numerator += Math.Pow((this._xData[i] - this._xBar), 2);
+            }
+
+            double sd = Math.Sqrt(numerator / (this._n - 1));
+
+            return sd;
+        }
+
+        public double ResidualSumofSquares()
+        {
+            double rss = 0;
+
+            for (int i = 0; i < this._n; i++)
+            {
+                rss += Math.Pow((this._yData[i] - this.betaZero - (this.betaOne * this._xData[i])), 2);
+            }
+
+            return rss;
+        }
+
+        private double TotalSumOfSquares()
+        {
+            double tss = 0;
+            for (int i = 0; i < this._n; i++)
+            {
+                tss += Math.Pow((this._yData[i] - this._yBar), 2);
+            }
+
+            return tss;
+        }
+        public double[] LSEStandardError()  // used for computing the standard error of least squares coefficients
+        {
+            double betaZeroSE = 0, betaOneSE = 0;
+            double denominator = 0;
+
+            // for betaOne (slope)
+            for (int i = 0; i < this._n; i++)
+            {
+                denominator += Math.Pow((this._xData[i] - this._xBar), 2);    
+            }
+            betaOneSE = Math.Pow(StandardDeviation(), 2) / denominator;
+
+            // for betaZero (y-intercept)
+            double equationOne = (1 / this._n) + ((Math.Pow(this._xBar, 2)) / (denominator));
+            betaZeroSE = Math.Pow(StandardDeviation(), 2) * equationOne;
+
+            return new double[] { betaOneSE, betaZeroSE };
+        }
+
+        public double TStatistic()
+        {
+            double[] se = LSEStandardError();
+
+            return this.betaOne / se[0];
+        }
+
+        public double ResidualStandardError()
+        {
+            return Math.Sqrt(ResidualSumofSquares() / (this._n - 2));
+        }
+
+        public double RSquared()
+        {
+            reuturn 1 - (ResidualSumofSquares() / TotalSumOfSquares());
         }
     }
 }
