84417105dc50a8c0b18eacfc28d0655bdc85be92
Vejay Drepaul <vejayddrepaul@gmail.com>
2023-12-28 23:46:36 -0500

    Can do multiple linear regression using matrices

---
+++ MultipleLinearRegression.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.Distributions;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+;
+
+namespace StatisticalModelling
+{
+    public class MultipleLinearRegression
+    {
+        Matrix<double> _xData;
+        Matrix<double> _yData;
+
+        public MultipleLinearRegression(Matrix<double> _xData, Matrix<double> _yData)
+        {
+            this._xData = _xData;
+            this._yData = _yData;
+        }
+
+        public double[,] RegressionCoefficients()
+        {
+            double[,] beta = (((this._xData.Transpose() * this._xData).Inverse()) * (this._xData.Transpose() * this._yData)).ToArray();
+
+            return beta;
+        }
+    }
+}
